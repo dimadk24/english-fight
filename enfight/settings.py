53f420d005356b9a4a967ba1a6ede61b288bc6da
Dima Krutolianov <DimaKrutDK@ya.ru>
2020-12-11 21:04:35 +0300

    fix: correctly take DEBUG from env

--- enfight/settings.py
+++ enfight/settings.py
@@ -30,7 +30,7 @@ environ.Env.read_env(str(dotenv_file))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = env('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = [
     '127.0.0.1'
