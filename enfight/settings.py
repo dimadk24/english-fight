cdde0f31edbce2a5f05a11629244c5cea5352007
Dima Krutolianov <DimaKrutDK@ya.ru>
2020-12-17 20:37:52 +0300

    feat: add autodeploy of dev server, get static root from env
    Add DJANGO_LOG_DIR to example

--- enfight/settings.py
+++ enfight/settings.py
@@ -150,7 +150,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = "static/"
+STATIC_ROOT = env("STATIC_ROOT", default=None)
 
 AUTH_USER_MODEL = "game.AppUser"
 
