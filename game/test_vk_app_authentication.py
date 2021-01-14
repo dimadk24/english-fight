b708ae183af98f36edce8347a756f53ecaa4b717
Dima Krutolianov <DimaKrutDK@ya.ru>
2021-01-14 22:54:25 +0300

    feat: save user vk data on the first request

--- game/test_vk_app_authentication.py
+++ game/test_vk_app_authentication.py
@@ -10,8 +10,10 @@ from rest_framework.exceptions import AuthenticationFailed
 from game.models import AppUser
 from game.vk_app_authentication import VKAppAuthentication
 
+photo_url = "https://test.com/image.png"
 
-@pytest.fixture(scope="module", autouse=True)
+
+@pytest.fixture(autouse=True)
 def mock_valid_query():
     mock_is_valid_query = mock.patch(
         "game.vk_app_authentication.VKAppAuthentication.is_valid_vk_query"
@@ -20,6 +22,21 @@ def mock_valid_query():
     VKAppAuthentication.is_valid_vk_query.return_value = False
     yield mock_is_valid_query
     mock_is_valid_query.stop()
+
+
+@pytest.fixture(autouse=True)
+def mock_get_vk_user_data():
+    mock_get_vk_data = mock.patch(
+        "game.vk_app_authentication.VKAppAuthentication.get_vk_user_data"
+    )
+    mock_get_vk_data.start()
+    VKAppAuthentication.get_vk_user_data.return_value = {
+        "first_name": "Cat",
+        "last_name": "Leo",
+        "photo_200": photo_url,
+    }
+    yield mock_get_vk_data
+    mock_get_vk_data.stop()
 
 
 def test_returns_none_when_no_auth_header():
@@ -70,8 +87,12 @@ def run_test_with_existing_user():
     user1 = AppUser.objects.get(vk_id=1)
     assert not user1.last_login
     user, query_params = VKAppAuthentication().authenticate(request)
+    VKAppAuthentication.get_vk_user_data.assert_called_once()
     user1 = AppUser.objects.get(vk_id=1)
     assert user == user1
+    assert user1.first_name == "Cat"
+    assert user1.last_name == "Leo"
+    assert user1.photo_url == photo_url
     now = timezone.now()
     assert now - user.last_login < timedelta(minutes=1)
     assert query_params == {
@@ -99,6 +120,7 @@ def run_test_with_new_user():
         "vk_app_id": "2",
     }
     user, _ = VKAppAuthentication().authenticate(request)
+    VKAppAuthentication.get_vk_user_data.assert_called_once()
     updated_user = AppUser.objects.get(vk_id=3)
     now = timezone.now()
     assert now - user.last_login < timedelta(minutes=1)
