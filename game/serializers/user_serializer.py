0868773a1277aadb0a7eb6a30766b434551f9478
Dima Krutolianov <DimaKrutDK@ya.ru>
2021-01-15 23:03:45 +0300

    feat: include user rank in /user view

--- game/serializers/user_serializer.py
+++ game/serializers/user_serializer.py
@@ -4,6 +4,8 @@ from game.models import AppUser
 
 
 class UserSerializer(serializers.ModelSerializer):
+    rank = serializers.IntegerField()
+
     class Meta:
         model = AppUser
-        fields = ("id", "vk_id", "score")
+        fields = ("id", "vk_id", "score", "rank")
