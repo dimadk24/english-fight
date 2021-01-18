60dc088fa014927ae8e0992d8a7d860463e81f37
Dima Krutolianov <DimaKrutDK@ya.ru>
2021-01-18 22:31:31 +0300

    fix: error when request /user when user doesn't have games

--- game/views/tests/test_user_view.py
+++ game/views/tests/test_user_view.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 from game.models import AppUser, Game
 
 
-def test_returns_current_user(api_client):
+def test_returns_current_user_when_have_games(api_client):
     users = [
         AppUser(vk_id=3, username="3", score=10),
         AppUser(vk_id=4, username="4", score=11),
@@ -57,6 +57,57 @@ def test_returns_current_user(api_client):
         "vk_id": 2,
         "score": 2,
         "forever_rank": 4,
+        "monthly_rank": 5,
+        "first_name": "cute",
+        "last_name": "cat",
+        "photo_url": "example.com/test.png",
+    }
+
+
+def test_returns_current_user_when_doesnt_have_games(api_client):
+    users = [
+        AppUser(vk_id=3, username="3", score=10),
+        AppUser(vk_id=4, username="4", score=11),
+        AppUser(vk_id=5, username="5", score=1),
+        AppUser(vk_id=6, username="6", score=2),
+    ]
+    AppUser.objects.bulk_create(users)
+    user = AppUser.objects.create(
+        vk_id=2,
+        username="2",
+        score=2,
+        first_name="cute",
+        last_name="cat",
+        photo_url="example.com/test.png",
+    )
+    Game.objects.create(
+        player=AppUser.objects.get(vk_id=6),
+        created_at=timezone.now(),
+        points=10,
+    )
+    Game.objects.create(
+        player=AppUser.objects.get(vk_id=5),
+        created_at=timezone.now(),
+        points=2,
+    )
+    Game.objects.create(
+        player=AppUser.objects.get(vk_id=4),
+        created_at=timezone.now(),
+        points=2,
+    )
+    Game.objects.create(
+        player=AppUser.objects.get(vk_id=3),
+        created_at=timezone.now(),
+        points=2,
+    )
+    api_client.force_authenticate(user)
+    response = api_client.get("/api/user")
+    assert response.status_code == 200
+    assert response.data == {
+        "id": user.id,
+        "vk_id": 2,
+        "score": 2,
+        "forever_rank": 3,
         "monthly_rank": 5,
         "first_name": "cute",
         "last_name": "cat",
