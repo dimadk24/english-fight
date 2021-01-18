bf2181cfea9b6c2da36171825428cc395ef8a569
Dima Krutolianov <DimaKrutDK@ya.ru>
2021-01-18 17:30:36 +0300

    fix: handle case when 2 users have same score in user_view

--- game/views/tests/test_user_view.py
+++ game/views/tests/test_user_view.py
@@ -1,4 +1,4 @@
-from game.models import AppUser
+from game.models import AppUser, Game
 
 
 def test_returns_current_user(api_client):
@@ -6,6 +6,7 @@ def test_returns_current_user(api_client):
         AppUser(vk_id=3, username="3", score=10),
         AppUser(vk_id=4, username="4", score=11),
         AppUser(vk_id=5, username="5", score=1),
+        AppUser(vk_id=6, username="6", score=2),
     ]
     AppUser.objects.bulk_create(users)
     user = AppUser.objects.create(
@@ -16,6 +17,9 @@ def test_returns_current_user(api_client):
         last_name="cat",
         photo_url="example.com/test.png",
     )
+    Game.objects.create(player=AppUser.objects.get(vk_id=2))
+    Game.objects.create(player=AppUser.objects.get(vk_id=2))
+    Game.objects.create(player=AppUser.objects.get(vk_id=5))
     api_client.force_authenticate(user)
     response = api_client.get("/api/user")
     assert response.status_code == 200
@@ -23,7 +27,7 @@ def test_returns_current_user(api_client):
         "id": user.id,
         "vk_id": 2,
         "score": 2,
-        "rank": 3,
+        "rank": 4,
         "first_name": "cute",
         "last_name": "cat",
         "photo_url": "example.com/test.png",
