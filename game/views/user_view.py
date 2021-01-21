9b9b4997a2a818aee140951b0e20f6e4870e28b0
Dima Krutolianov <DimaKrutDK@ya.ru>
2021-01-21 21:58:21 +0300

    refactor: simplify getting ranks

--- game/views/user_view.py
+++ game/views/user_view.py
@@ -1,8 +1,5 @@
-from django.db.models import Count, Q, Sum
-from django.utils import timezone
 from rest_framework.generics import RetrieveAPIView
 
-from game.models import AppUser
 from game.serializers.user_serializer import UserSerializer
 
 
@@ -10,56 +7,4 @@ class UsersView(RetrieveAPIView):
     serializer_class = UserSerializer
 
     def get_object(self):
-        user = self.request.user
-        user_games_count = user.game_set.count()
-        annotated_users = AppUser.users.annotate(Count("game"))
-        forever_rank = (
-            annotated_users.filter(score__gt=user.score)
-            .union(
-                annotated_users.filter(
-                    Q(score=user.score) & Q(game__count__lt=user_games_count),
-                ),
-                annotated_users.filter(
-                    Q(score=user.score)
-                    & Q(game__count=user_games_count)
-                    & Q(vk_id__lt=user.vk_id),
-                ),
-            )
-            .count()
-            + 1
-        )
-        current_month = timezone.now().month
-        monthly_score = Sum(
-            "game__points", filter=Q(game__created_at__month=current_month)
-        )
-        annotated_users = annotated_users.annotate(monthly_score=monthly_score)
-        user_monthly_score = (
-            annotated_users.get(vk_id=user.vk_id).monthly_score or 0
-        )
-        monthly_rank = (
-            annotated_users.filter(monthly_score__gt=user_monthly_score)
-            .union(
-                annotated_users.filter(
-                    Q(monthly_score=user.score)
-                    & Q(game__count__lt=user_games_count),
-                ),
-                annotated_users.filter(
-                    Q(monthly_score=user.score)
-                    & Q(game__count=user_games_count)
-                    & Q(vk_id__lt=user.vk_id),
-                ),
-            )
-            .count()
-            + 1
-        )
-        return {
-            "id": user.id,
-            "score": user.score,
-            "vk_id": user.vk_id,
-            "forever_rank": forever_rank,
-            "monthly_rank": monthly_rank,
-            "first_name": user.first_name,
-            "last_name": user.last_name,
-            "photo_url": user.photo_url,
-            "notifications_status": user.notifications_status,
-        }
+        return self.request.user
