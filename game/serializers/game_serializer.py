db7b4d92af9db1cc49968f8a5a1594a8c127edf1
Dima Krutolianov <DimaKrutDK@ya.ru>
2021-01-25 10:43:47 +0300

    feat: add type field to game

--- game/serializers/game_serializer.py
+++ game/serializers/game_serializer.py
@@ -12,5 +12,5 @@ class GameSerializer(FlexFieldsModelSerializer):
 
     class Meta:
         model = Game
-        fields = ["id", "player", "questions", "points"]
+        fields = ["id", "type", "player", "questions", "points"]
         expandable_fields = {"questions": (QuestionSerializer, {"many": True})}
