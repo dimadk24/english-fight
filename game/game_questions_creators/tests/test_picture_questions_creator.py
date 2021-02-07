902675573f637a389dc2d9f9eb85a42a2d6fd423
Dima Krutolianov <DimaKrutDK@ya.ru>
2021-02-07 16:15:00 +0300

    test: ensure that there are enough items in each picture topic

--- game/game_questions_creators/tests/test_picture_questions_creator.py
+++ game/game_questions_creators/tests/test_picture_questions_creator.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from django.conf import settings
 
 from data.pictures import PICTURES
+from game.constants import QUESTIONS_PER_GAME, ANSWERS_PER_QUESTION
 from game.game_questions_creators.picture_questions_creator import (
     get_file_path,
 )
@@ -18,3 +19,11 @@ def test_get_file_path():
             )
             full_path: Path = settings.BASE_DIR / "data" / file_path
             assert full_path.exists()
+
+
+def test_ensure_each_topic_has_enough_items():
+    assert len(PICTURES)
+    for picture_topic in PICTURES:
+        items_number = len(picture_topic.items)
+        assert items_number >= QUESTIONS_PER_GAME
+        assert items_number >= ANSWERS_PER_QUESTION
