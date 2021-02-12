5037de8c6a5008ad11151b21cfded650c4b97561
Dima Krutolianov <DimaKrutDK@ya.ru>
2021-02-12 20:08:24 +0300

    feat: create views to match new flow of game creation
    Also update tests

--- game/urls.py
+++ game/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
+from game.views.game_definition_view import GameDefinitionView
 from game.views.game_view import GameView
 from game.views.question_view import QuestionView
 from game.views.scoreboard_view import ScoreboardView, ScoreboardType
@@ -7,6 +8,7 @@ from game.views.user_view import UsersView
 
 urlpatterns = [
     path("user", UsersView.as_view()),
+    path("game_definition", GameDefinitionView.as_view()),
     path("game", GameView.as_view()),
     path("game/<int:pk>", GameView.as_view()),
     path("question/<int:pk>", QuestionView.as_view()),
--- game/views/tests/test_game_view.py
+++ game/views/tests/test_game_view.py
@@ -10,10 +10,16 @@ from rest_framework.test import APIClient
 from conftest import get_correct_answer_data_for_picture_question
 from data.word_pairs import get_pair_by_english_word
 from game.constants import QUESTIONS_PER_GAME
-from game.models import AppUser, Game, Question
+from game.models import AppUser, Game, Question, GameDefinition
 
 
-def post(api_client: APIClient, data=None) -> Response:
+def post_game_definition(api_client: APIClient, data=None) -> Response:
+    if data is None:
+        data = {}
+    return api_client.post("/api/game_definition", data)
+
+
+def post_game(api_client: APIClient, data=None) -> Response:
     if data is None:
         data = {}
     return api_client.post("/api/game", data)
@@ -35,12 +41,23 @@ def assert_not_included_in_questions(word: str):
 
 
 def do_database_asserts(game_type: str):
+    current_user = AppUser.objects.get(vk_id=1)
+
+    assert GameDefinition.objects.count() == 1
+    game_definition: GameDefinition = GameDefinition.objects.get()
+    assert game_definition.creator == current_user
+    assert list(game_definition.players.all()) == [current_user]
+    assert game_definition.type == game_type
+    now = timezone.now()
+    assert now - game_definition.created_at < timedelta(minutes=1)
+
     assert Game.objects.count() == 1
     assert Question.objects.count() == 5
 
     game = Game.objects.get()
+    assert game.game_definition == game_definition
     assert game.questions.count() == 5
-    assert game.player == AppUser.objects.get(vk_id=1)
+    assert game.player == current_user
     assert game.points == 0
     now = timezone.now()
     assert now - game.created_at < timedelta(minutes=1)
@@ -51,10 +68,10 @@ def do_database_asserts(game_type: str):
 
     for question in game.questions.all():
         assert question.correct_answer in question.answer_words
-        if game_type == Game.WORD:
+        if game_type == GameDefinition.WORD:
             language_pair = get_pair_by_english_word(question.question)
             assert language_pair["russian_word"] == question.correct_answer
-        elif game_type == Game.PICTURE:
+        elif game_type == GameDefinition.PICTURE:
             topic, item = get_correct_answer_data_for_picture_question(
                 question
             )
@@ -72,18 +89,24 @@ def do_database_asserts(game_type: str):
     assert_not_included_in_questions("Zero")
 
 
-# run it many times just to ensure it always passes
+# run it many times just to ensure it always passes, cause it uses random
 @pytest.mark.parametrize("i", range(20))
 @pytest.mark.parametrize("game_type", ["word", "picture"])
 def test_creates_with_real_random(i: int, game_type, api_client):
-    response = post(api_client, {"type": game_type})
+    game_definition_response = post_game_definition(
+        api_client, {"type": game_type}
+    )
+    assert game_definition_response.status_code == 201
 
-    assert response.status_code == 201
-    question_ids = response.data["questions"]
+    game_response = post_game(
+        api_client, {"game_definition": game_definition_response.data["id"]}
+    )
+
+    assert game_response.status_code == 201
+    question_ids = game_response.data["questions"]
     assert len(question_ids) == QUESTIONS_PER_GAME
     for question_id in question_ids:
         assert Question.objects.filter(pk=question_id).exists()
-    assert response.data["type"] == game_type
     do_database_asserts(game_type)
 
 
@@ -141,13 +164,30 @@ def test_creates_with_fake_random(get_random_int, game_type, api_client):
         0,
         0,
     ]
-    response = post(api_client, {"type": game_type})
-    assert response.status_code == 201
+    game_definition_response = post_game_definition(
+        api_client, {"type": game_type}
+    )
+    assert game_definition_response.status_code == 201
+    game_def = game_definition_response.data
+    assert game_def["type"] == game_type
+    assert isinstance(game_def["id"], str)
+    assert isinstance(game_def["creator"], int)
+    assert isinstance(game_def["players"], list)
+
+    game_response = post_game(api_client, {"game_definition": game_def["id"]})
+
+    question_ids = game_response.data["questions"]
+    assert len(question_ids) == QUESTIONS_PER_GAME
+    for question_id in question_ids:
+        assert Question.objects.filter(pk=question_id).exists()
+
+    assert game_response.status_code == 201
     do_database_asserts(game_type)
 
 
 def test_returns_game_of_current_user(api_client):
-    game = post(api_client).data
+    game_def = post_game_definition(api_client, {"type": "word"})
+    game = post_game(api_client, {"game_definition": game_def.data["id"]}).data
     response = get(api_client, game["id"])
 
     assert response.status_code == 200
@@ -158,7 +198,10 @@ def test_returns_game_of_current_user(api_client):
 
 
 def test_does_not_return_game_of_another_user(api_client):
-    game = post(api_client).data
+    game_def_response = post_game_definition(api_client, {"type": "word"})
+    game = post_game(
+        api_client, {"game_definition": game_def_response.data["id"]}
+    ).data
 
     user2 = AppUser.objects.create(vk_id=2, username=2)
     api_client.force_authenticate(user2)
@@ -166,3 +209,33 @@ def test_does_not_return_game_of_another_user(api_client):
 
     assert response.status_code == 404
     assert response.data["detail"] == "Страница не найдена."
+
+
+def test_does_not_allow_to_set_creator(api_client):
+    first_user = AppUser.objects.create(vk_id=2, username=2)
+    api_client.force_authenticate(first_user)
+    second_user = AppUser.objects.create(vk_id=3, username=3)
+
+    game_def_response = post_game_definition(
+        api_client, {"type": "word", "creator": second_user.id}
+    )
+    assert game_def_response.status_code == 201
+
+    game_def = GameDefinition.objects.get(id=game_def_response.data["id"])
+    assert game_def.creator_id == first_user.id
+    assert game_def_response.data["creator"] == first_user.id
+
+
+def test_does_not_allow_to_set_players(api_client):
+    first_user = AppUser.objects.create(vk_id=2, username=2)
+    api_client.force_authenticate(first_user)
+    second_user = AppUser.objects.create(vk_id=3, username=3)
+
+    game_def_response = post_game_definition(
+        api_client, {"type": "word", "players": [second_user.id]}
+    )
+    assert game_def_response.status_code == 201
+
+    game_def = GameDefinition.objects.get(id=game_def_response.data["id"])
+    assert list(game_def.players.all()) == [first_user]
+    assert game_def_response.data["players"] == [first_user.id]
