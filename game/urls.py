6a21c7e41cf4981a52c8777e935f6542272c2330
Dima Krutolianov <DimaKrutDK@ya.ru>
2021-02-17 14:31:51 +0300

    feat: get game definition url

--- game/urls.py
+++ game/urls.py
@@ -9,6 +9,7 @@ from game.views.user_view import UsersView
 urlpatterns = [
     path("user", UsersView.as_view()),
     path("game_definition", GameDefinitionView.as_view()),
+    path("game_definition/<str:pk>", GameDefinitionView.as_view()),
     path("game", GameView.as_view()),
     path("game/<int:pk>", GameView.as_view()),
     path("question/<int:pk>", QuestionView.as_view()),
