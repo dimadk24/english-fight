e2340c29b734dcbf9dde2a765e8bf0472535003f
Dima Krutolianov <DimaKrutDK@ya.ru>
2021-02-17 14:31:51 +0300

    feat: get game definition url

--- game/views/game_definition_view.py
+++ game/views/game_definition_view.py
@@ -8,8 +8,4 @@ from game.serializers.game_definition_serializer import (
 
 class GameDefinitionView(CreateAPIView, RetrieveAPIView):
     serializer_class = GameDefinitionSerializer
-
-    def get_queryset(self):
-        return GameDefinition.objects.filter(
-            players__contains=self.request.user
-        )
+    queryset = GameDefinition.objects.all()
