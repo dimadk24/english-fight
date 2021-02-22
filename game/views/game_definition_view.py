8991250f6c6c4d6f06fee2a69d07669f31b3154d
Dima Krutolianov <DimaKrutDK@ya.ru>
2021-02-22 20:40:36 +0300

    feat: reject to join the game if it's already started

--- game/views/game_definition_view.py
+++ game/views/game_definition_view.py
@@ -1,3 +1,4 @@
+from rest_framework.exceptions import PermissionDenied
 from rest_framework.generics import CreateAPIView, RetrieveAPIView
 
 from game.models import GameDefinition
@@ -9,3 +10,9 @@ from game.serializers.game_definition_serializer import (
 class GameDefinitionView(CreateAPIView, RetrieveAPIView):
     serializer_class = GameDefinitionSerializer
     queryset = GameDefinition.objects.all()
+
+    def get_object(self):
+        instance = super().get_object()
+        if instance.started:
+            raise PermissionDenied(detail='К игре уже нельзя подключиться')
+        return instance
