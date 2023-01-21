589b95054702e148d6ae163731f7f684330373b9
Dima Krutolianov <DimaKrutDK@ya.ru>
2023-01-21 20:43:59 +0100

    test: specify device to use for e2e more correctly

--- backend/tests/e2e/conftest.py
+++ backend/tests/e2e/conftest.py
@@ -3,11 +3,6 @@ import os
 import pytest
 from playwright.async_api import async_playwright
 
-VIEWPORT = {
-    'width': 390,
-    'height': 844,
-}  # iPhone 12
-
 
 @pytest.fixture(scope="session", autouse=True)
 def allow_using_sync_playwright_api():
@@ -21,18 +16,16 @@ def allow_using_sync_playwright_api():
     os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = previous_value
 
 
-@pytest.fixture(scope="session")
-def browser_context_args(browser_context_args):
-    """Set viewport size for all tests."""
-    return {**browser_context_args, "viewport": VIEWPORT}
-
-
 @pytest.fixture()
 async def async_page(browser_name, browser_type_launch_args):
     async with async_playwright() as p:
         browser_type = getattr(p, browser_name)
         browser = await browser_type.launch(**browser_type_launch_args)
         page = await browser.new_page()
-        await page.set_viewport_size(VIEWPORT)
+        viewport = {
+            'width': 390,
+            'height': 664,
+        }  # iPhone 12
+        await page.set_viewport_size(viewport)
         yield page
         await browser.close()
