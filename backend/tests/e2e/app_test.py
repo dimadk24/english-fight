92b1925ac741613bb95fc653962b17982bb877fa
Dima Krutolianov <DimaKrutDK@ya.ru>
2023-02-02 20:21:35 +0100

    test: add test for picture game
    set random seed before test instead of printing random state
    screenshot question

--- backend/tests/e2e/app_test.py
+++ backend/tests/e2e/app_test.py
@@ -1,18 +1,29 @@
+import random
+
+import pytest
 from playwright.sync_api import Page, expect
 
 from enfight.settings import BASE_DIR
 from game.models import GameDefinition
 from game.test_question_utils import get_correct_answer_to_question
-from test_utils import print_random_state
 
 screenshot_base_path = BASE_DIR / "tests" / "e2e" / "screenshots"
 
+game_type_to_game_name = {
+    GameDefinition.WORD: "Перевод",
+    GameDefinition.PICTURE: "Картинка",
+}
 
-def test_word_game(live_server, page: Page):
-    print_random_state()
+
+@pytest.mark.parametrize(
+    "game_type", [GameDefinition.WORD, GameDefinition.PICTURE]
+)
+def test_game(live_server, page: Page, game_type):
+    random.seed(2)
 
     def screenshot(name):
-        page.screenshot(path=screenshot_base_path / f"{name}.png")
+        path = screenshot_base_path / f"{game_type}-game" / f"{name}.png"
+        page.screenshot(path=path)
 
     page.goto(
         "http://localhost:3000/english-fight?fake_vk_id=374637778",
@@ -28,15 +39,25 @@ def test_word_game(live_server, page: Page):
 
     screenshot("choose_game_type")
 
-    page.get_by_role("button", name="Перевод").click()
+    game_type_name = game_type_to_game_name[game_type]
+    translate_game_button = page.get_by_role("button", name=game_type_name)
+    translate_game_button.click()
+    translate_game_button.wait_for(state="hidden")  # for screenshot
+
+    screenshot("question")
 
     for _ in range(0, 10):
-        question_obj = page.get_by_role("heading")
-        question = question_obj.inner_text()
-
-        correct_answer = get_correct_answer_to_question(
-            question, GameDefinition.WORD
-        )
+        if game_type == GameDefinition.WORD:
+            question_obj = page.get_by_role("heading")
+            question = question_obj.inner_text()
+            full_question = question
+            attribute = "innerText"
+        else:
+            img = page.get_by_role("img", name="Картинка с вопросом")
+            full_question = img.get_attribute("src")
+            question = full_question.replace(live_server.url, "")
+            attribute = "src"
+        correct_answer = get_correct_answer_to_question(question, game_type)
 
         page.get_by_role("button", name=correct_answer, exact=True).click()
         page.wait_for_function(
@@ -44,14 +65,14 @@ def test_word_game(live_server, page: Page):
         function() {
           let found = false
           for (const el of document.querySelectorAll("*")) {
-            if (el.textContent === "%s") {
+            if (el.%s === "%s") {
               found = true
             }
           }
           return !found
         }
         """
-            % question
+            % (attribute, full_question)
         )
 
     not_now_button = page.get_by_role("button", name="Не сейчас")
