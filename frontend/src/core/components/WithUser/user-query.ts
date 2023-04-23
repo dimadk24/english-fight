55666cf248729ab64d2034c5310c76e3e304d1ed
Dima Krutolianov <DimaKrutDK@ya.ru>
2023-04-23 10:59:17 +0200

    refactor: use react-query for user data management

---
+++ frontend/src/core/components/WithUser/user-query.ts
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import { useQuery } from '@tanstack/react-query'
+import { AppService } from '../../../AppService'
+import { queryClient } from '../../../react-query-client'
+import { UserInstance } from '../../user-model'
+
+const userQueryKey = ['user']
+
+export const useUserQuery = () => {
+  return useQuery({
+    queryKey: userQueryKey,
+    queryFn: AppService.fetchUserData,
+  })
+}
+
+export const setUserData = (user: UserInstance) => {
+  return queryClient.setQueryData(userQueryKey, user)
+}
+
+export const invalidateUser = () =>
+  queryClient.invalidateQueries({ queryKey: userQueryKey })
