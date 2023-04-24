2b6ade07c6724efa3b29930657dc35d8f3485383
Dima Krutolianov <DimaKrutDK@ya.ru>
2023-04-24 21:05:06 +0200

    fix(e2e): disable eruda and react-query devtools
    Also, remove screenshots folder in e2e tests script

--- scripts/run_e2e_tests.sh
+++ scripts/run_e2e_tests.sh
@@ -2,6 +2,9 @@
 
 FRONTEND_URL="http://localhost:3000/english-fight"
 
+# Clean up screenshots to ensure to remove not used screenshots
+yarn --cwd frontend rimraf ../backend/tests/e2e/screenshots
+
 yarn --cwd=frontend start:e2e &
 # wait till frontend compiles up to 3 min, with delay 10s (as it takes time) and interval 1s
 yarn --cwd=frontend wait-on "$FRONTEND_URL" --delay 10000 --timeout 180000 --interval 1000 &&
