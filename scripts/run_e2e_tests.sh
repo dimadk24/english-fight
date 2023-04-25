5b661e32914430319e0c76de4161589296c3cbdc
Dima Krutolianov <DimaKrutDK@ya.ru>
2023-04-25 19:43:28 +0200

    fix: set DEBUG=0 for backend tests

--- scripts/run_e2e_tests.sh
+++ scripts/run_e2e_tests.sh
@@ -8,6 +8,6 @@ yarn --cwd frontend rimraf ../backend/tests/e2e/screenshots
 yarn --cwd=frontend start:e2e &
 # wait till frontend compiles up to 3 min, with delay 10s (as it takes time) and interval 1s
 yarn --cwd=frontend wait-on "$FRONTEND_URL" --delay 10000 --timeout 180000 --interval 1000 &&
-  (cd backend && poetry run pytest tests/e2e)
+  (cd backend && IS_E2E_TESTS=1 poetry run pytest tests/e2e)
 #  braces to don't change current shell directory
 #  See https://stackoverflow.com/a/786419/7119080
